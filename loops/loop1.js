90565d870ce42db7a778973165e8b3dd670e162f
harunazakaria <Harunzy55@gmail.com>
2023-08-16 00:11:11 -0700

    add objectsand loops

---
+++ loops/loop1.js
@@ -0,0 +1,15 @@
+/*
+You can run the same code multiple times by using a loop.
+
+The first type of loop we will learn is called 
+a while loop because it runs while a specified 
+condition is true and stops once that condition is no longer true.
+*/
+const ourArray = [];
+let i = 0;
+
+while (i < 5) {
+  ourArray.push(i);
+  i++;
+}
+console.log(ourArray);
