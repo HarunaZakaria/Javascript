2d48fda111394c141f159d9d733dae5ff444845a
harunazakaria <Harunzy55@gmail.com>
2023-08-23 08:54:18 +0100

    add all changes

---
+++ varLet/spread.js
@@ -0,0 +1,5 @@
+//Copy all contents of arr1 into another array arr2 using the spread operator.
+const name = ['haruna', 'zakaria', 'suale', 'dauda', 4];
+let family;
+family = [...name]
+console.log(family);
---
+++ varLet/var1.js
@@ -0,0 +1,6 @@
+var numArray = [];
+for (var i = 0; i < 3; i++) {
+  numArray.push(i);
+}
+console.log(numArray);
+console.log(i);
