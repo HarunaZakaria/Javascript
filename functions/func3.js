5447ea28ef053f09b7de4191ccebba1fe81bf18b
harunazakaria <Harunzy55@gmail.com.com>
2024-01-31 01:27:00 +0000

    add themultiAdd function

---
+++ functions/func3.js
@@ -0,0 +1,7 @@
+// a write a function that takes two numbers multiply them and divide them by it addition
+function multiAdd(num1, num2) {
+  return (num1 * num2) / (num1 + num2);
+}
+
+let n = multiAdd(5, 10);
+console.log(n);
