cb573ad24ac2ac96a71d069ce27b06ed8d72a9fa
Sir Harunzy <Harunzy55@gmail.com>
2025-03-08 00:59:25 -0800

    add  js objects

---
+++ objects/index.js
@@ -0,0 +1,9 @@
+let personOne = {
+    name: "Harunzy",
+    age: 26,
+    educationLlevel: "College",
+    languageSppoken: ["English", "Dagbani", "Twi"]
+}
+
+let greet = "Good Morning " + personOne.name;
+alert(greet);
