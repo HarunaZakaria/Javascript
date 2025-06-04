80788f8880c81aedd94104515063e6b737a062bc
Sir Harunzy <Harunzy55@gmail.com>
2025-06-04 01:04:02 +0000

    add new directory

---
+++ amazon/front-end/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <header>
+        <a href="/">Amazon</a>
+      </header>
+      <main>list products</main>
+    </>
+  );
+}
+
+export default App;
