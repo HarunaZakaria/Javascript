4badb6fb349c823fa7ef66ba2c36b226d8567578
Sir Harunzy <Harunzy55@gmail.com>
2025-06-19 23:36:07 +0000

    add an error code

---
+++ webdev/Js/Error/index.js
@@ -0,0 +1,8 @@
+try {
+  console.log(a + b);
+} catch (error) {
+  console.log(error);
+  console.log('there was an error');
+  console.log('it was save in the error log');
+}
+console.log('the program is still running');
