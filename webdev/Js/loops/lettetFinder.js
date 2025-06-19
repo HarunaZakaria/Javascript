b3ea1c3282642157397df60860cac3dae2248a6c
Sir Harunzy <Harunzy55@gmail.com>
2025-06-19 20:54:16 +0000

    add letter finder project

---
+++ webdev/Js/loops/lettetFinder.js
@@ -0,0 +1,12 @@
+//write a  function which will be able to take a word and locate the position of a chosen letter in that given word.
+
+function letterFinder(word, match) {
+  for (let i = 0; i < word.length; i++) {
+    if (word[i] == match) {
+      console.log('Found the', match, 'at', i);
+    } else {
+      console.log('---No match found at', i);
+    }
+  }
+}
+letterFinder('test', 't');
