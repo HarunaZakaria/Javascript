ace1d556c4f2822afacaef3a45f38addc005be0e
Sir Harunzy <Harunzy55@gmail.com>
2025-06-19 17:26:41 +0000

    add

---
+++ webdev/Js/index.js
@@ -0,0 +1,12 @@
+//nested fo loop
+for (let i = 1; i <= 10; i++) {
+  if (i === 1) {
+    console.log('Gold medal');
+  } else if (i === 2) {
+    console.log('Silver medal');
+  } else if (i === 3) {
+    console.log('Bronze medal');
+  } else {
+    console.log(i);
+  }
+}
