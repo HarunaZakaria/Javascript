f9d65e671eb98fde898b8c4550f4b40c72ee5a0c
Sir Harunzy <Harunzy55@gmail.com>
2025-06-20 00:53:48 +0000

    add a new error handle

--- webdev/Js/Error/index.js
+++ webdev/Js/Error/index.js
@@ -5,4 +5,21 @@ try {
   console.log('there was an error');
   console.log('it was save in the error log');
 }
-console.log('the program is still running');
+//console.log('the program is still running');
+
+//working error handler
+function addTwoNums(a, b) {
+  try {
+    if (typeof a != 'number') {
+      throw new TypeError('The first argument is not a number.');
+    } else if (typeof b != 'number') {
+      throw new TypeError('The second argument is not a number');
+    }
+    console.log(a + b);
+  } catch (err) {
+    console.log('Error!', err);
+  }
+}
+
+addTwoNums('4', 4);
+console.log('It still works');
