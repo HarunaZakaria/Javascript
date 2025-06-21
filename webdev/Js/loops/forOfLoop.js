9170d84453ef36e92d06376f6fe986ad19c8fbb8
Sir Harunzy <Harunzy55@gmail.com>
2025-06-21 11:27:37 +0000

    add more

--- webdev/Js/loops/forOfLoop.js
+++ webdev/Js/loops/forOfLoop.js
@@ -1,4 +1,20 @@
 let names = ['Haruna', 'Zeinab Nurse', 'Zeinab Tailor', 'Ruhia', 'Mom'];
 for (let name of names) {
-  console.log(name);
+  //console.log(name);
 }
+
+let dairy = [
+  'cheese',
+  'sour cream',
+  'milk',
+  'yogurt',
+  'ice cream',
+  'milkshake',
+];
+function logDairy() {
+  for (let dair of dairy) {
+    console.log(dair);
+  }
+}
+//output each daily
+//logDairy();
