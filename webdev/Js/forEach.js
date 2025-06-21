d1d3ecd55fc9f554b7c524fcd7c7f61dbfc49c7e
Sir Harunzy <Harunzy55@gmail.com>
2025-06-21 15:05:51 +0000

    add

--- webdev/Js/forEach.js
+++ webdev/Js/forEach.js
@@ -1,5 +1,15 @@
 const names = ['Haruna', 'Soaliho', 'Dauda', 'Napari', 'Hamis', 'Aisha'];
 // out each name with it corresponding idex
 names.forEach((name, index) => {
-  console.log(`at ${index} the name is ${name}`);
+  //console.log(`at ${index} the name is ${name}`);
 });
+
+const nums1 = [0, 10, 20, 30, 40, 50];
+nums1.filter(function (num) {
+  return num > 20;
+  console.log(result);
+});
+
+const repetitiveFruits = ['apple', 'pear', 'apple', 'pear', 'plum', 'apple'];
+const uniqueFruits = new Set(repetitiveFruits);
+//console.log(uniqueFruits);
