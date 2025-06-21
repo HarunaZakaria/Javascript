442b0dc80728dde87aa84eb346d08b3dbfbfcc7e
Sir Harunzy <Harunzy55@gmail.com>
2025-06-21 10:45:43 +0000

    add

--- webdev/Js/oopProgram.js
+++ webdev/Js/oopProgram.js
@@ -1,4 +1,4 @@
-// Task 1: Code a Person class
+// create a person class
 class Person {
   constructor(name = 'Tom', age = 20, energy = 100) {
     this.name = name;
@@ -15,7 +15,6 @@ class Person {
   }
 }
 
-// Task 2: Code a Worker class
 class Worker extends Person() {
   constructor(xp = 0, hourlyWage = 10, name, age, energy) {
     this.xp = xp;
@@ -28,18 +27,4 @@ class Worker extends Person() {
     this.xp += 10;
     console.log('your experience has been increased by: ', this.xp);
   }
-}
-
-// Task 3: Code an intern object, run methods
-function intern() {
-  let intern = new worker(0, 10, 'Bob', 21, 110);
-  intern.goToWork();
-  return intern;
-}
-
-// Task 4: Code a manager object, methods
-function manager() {
-  let manager = new Worker(100, 30, 'Alice', 30, 120);
-  manager.doSomethingFun();
-  return manager;
 }
