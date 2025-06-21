e949c507c881f0e9c6e411414d6b209116dffbd7
Sir Harunzy <Harunzy55@gmail.com>
2025-06-21 15:22:48 +0000

    add

--- webdev/Js/spread_rest.js
+++ webdev/Js/spread_rest.js
@@ -1,5 +1,22 @@
+//rest operator
 const [first, ...rest] = [1, 2, 3, 4];
 
-console.log(first); // 1
+//console.log(first); // 1
 
-console.log(rest); // [2, 3, 4]
+//console.log(rest); // [2, 3, 4]
+
+//spread operator
+function sum(...numbers) {
+  return numbers.reduce((total, num) => total + num, 0);
+}
+
+//console.log(sum(1, 2, 3)); // 6
+
+const fruits = ['apple', 'pear', 'plum'];
+const berries = ['blueberry', 'strawberry'];
+const fruitsAndBerries = [...fruits, ...berries]; // concatenate
+//console.log(fruitsAndBerries); // outputs a single array
+
+const greeting = 'Hello';
+const arrayOfChars = [...greeting];
+console.log(arrayOfChars); //  ['H', 'e', 'l', 'l', 'o']
