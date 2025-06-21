5fd37714f412798638666f964292bf19afaff833
Sir Harunzy <Harunzy55@gmail.com>
2025-06-21 11:36:56 +0000

    add again

--- webdev/Js/loops/forOfLoop.js
+++ webdev/Js/loops/forOfLoop.js
@@ -18,3 +18,19 @@ function logDairy() {
 }
 //output each daily
 //logDairy();
+
+const animal = {
+  canJump: true,
+};
+
+const bird = Object.create(animal);
+bird.canFly = true;
+bird.hasFeathers = true;
+
+function birdCan() {
+  for (let key of Object.keys(bird)) {
+    console.log(key, ':', bird[key]);
+  }
+}
+
+birdCan();
