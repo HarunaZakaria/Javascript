4f4cccb60a865e9a07884020c60bc26bd3ffe564
Sir Harunzy <Harunzy55@gmail.com>
2025-06-21 14:23:28 +0000

    add forEach method

---
+++ webdev/Js/forEach.js
@@ -0,0 +1,5 @@
+const names = ['Haruna', 'Soaliho', 'Dauda', 'Napari', 'Hamis', 'Aisha'];
+// out each name with it corresponding idex
+names.forEach((name, index) => {
+  console.log(`at ${index} the name is ${name}`);
+});
