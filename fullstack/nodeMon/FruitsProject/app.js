972742aa4c35bd009da258f03d7748c2d80cb32c
Sir Harunzy <Harunzy55@gmail.com>
2025-08-21 12:08:06 +0000

    add database

--- fullstack/nodeMon/FruitsProject/app.js
+++ fullstack/nodeMon/FruitsProject/app.js
@@ -1,22 +1,54 @@
-const { MongoClient } = require('mongodb');
+const mongoose = require('mongoose');
 
-// Replace the uri string with your connection string
-const uri = 'mongodb://localhost:27017';
+main().catch((err) => console.log(err));
 
-const client = new MongoClient(uri);
-
-async function run() {
-  try {
-    const database = client.db('fruitDb');
-    const movies = database.collection('movies');
-
-    // Queries for a movie that has a title value of 'Back to the Future'
-    const query = { title: 'Back to the Future' };
-    const movie = await movies.findOne(query);
-
-    console.log(movie);
-  } finally {
-    await client.close();
-  }
+async function main() {
+  await mongoose.connect('mongodb://127.0.0.1:27017/fruitsDB');
 }
-run().catch(console.dir);
+
+const fruitsSchema = new mongoose.Schema({
+  name: String,
+  rating: Number,
+  review: String,
+});
+
+const Fruit = mongoose.model('Fruit', fruitsSchema);
+
+const fruit = new Fruit({
+  name: 'Banana',
+  rating: 8,
+  review: 'Nice fruit',
+});
+//add only one fruits
+//fruit.save();
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  age: Number,
+});
+
+const Person = mongoose.model('Person', personSchema);
+
+const person = new Person({
+  name: 'Harunzy',
+  age: 26,
+});
+
+//add only one name
+//person.save()
+
+const Daud = new Person({
+  name: 'Daud',
+  age: 32,
+});
+const Hamis = new Person({
+  name: 'Hamis',
+  age: 30,
+});
+const Alhaj = new Person({
+  name: 'Alhaj',
+  age: 35,
+});
+
+//add many names
+Person.insertMany([Daud, Hamis, Alhaj]);
